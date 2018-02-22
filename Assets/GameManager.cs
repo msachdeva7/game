a105639ad3ad93cff12a95561b9f41a16b6bc24a
Matthew Joyce <matsjoyce@gmail.com>
2018-02-22 15:57:41 +0000

    Expand zip

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("r")) 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void EndGame()
+    {
+        
+    }
+}
---
+++ Assets/player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed *= Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        if (Input.GetKey("d"))
+            rb.AddForce(speed, 0, 0);
+        if (Input.GetKey("a"))
+            rb.AddForce(-speed, 0, 0);
+        if (Input.GetKey("w"))
+            rb.AddForce(0, 0, speed);
+        if (Input.GetKey("s"))
+            rb.AddForce(0, 0, -speed);
+    }
+}

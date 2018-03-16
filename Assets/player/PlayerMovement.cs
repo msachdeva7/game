7def9fcc443f19c27e19d713f747f9439a23c9e0
Matthew Joyce <matsjoyce@gmail.com>
2018-03-16 14:35:06 +0000

    Fix speed counter and danger, both of which were incorrectly implemented

--- Assets/player/PlayerMovement.cs
+++ Assets/player/PlayerMovement.cs
@@ -5,24 +5,19 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
     public Rigidbody rb;
-	public float drive_force;
+    public float drive_force;
     bool waitingForCommands = false;
     int frames = 0;
-	public Text speedText;
-	private Vector3 v;
-	//danger monitors if an object is nearby within an angle of +-15 deg in front of the car
-	public bool danger;
+    public Text speedText;
+    //danger monitors if an object is nearby within an angle of +-15 deg in front of the car
+    public bool danger = false;
 
     GameManager gm;
 
     // Use this for initialization
     void Start() {
-		drive_force *= Time.fixedDeltaTime;
-		v.x = 0.0f;
-		v.z = 0.0f;
-		danger = false;
-
-		update_speed ();
+        drive_force *= Time.fixedDeltaTime;
+        update_speed();
 
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
         if (gm == null) {
@@ -32,45 +27,42 @@ public class PlayerMovement : MonoBehaviour {
 
     void FixedUpdate() {
         frames++;
-        if (waitingForCommands && gm.inter.HasCommands()) {
+        if (gm.inter.HasCommands()) {
             waitingForCommands = false;
 
             PlayerCommands cmds = gm.inter.GetCommands();
 
             if (cmds.forward) {
-				rb.AddForce(drive_force*Vector3.forward);
-				v.z += (drive_force / rb.mass) * Time.fixedDeltaTime;
+                rb.AddForce(drive_force*Vector3.forward);
             }
             if (cmds.backward) {
-				rb.AddForce(drive_force*Vector3.back);
-				v.z -= (drive_force / rb.mass) * Time.fixedDeltaTime;
+                rb.AddForce(drive_force*Vector3.back);
             }
             if (cmds.left) {
-				rb.AddForce(drive_force*Vector3.left);
-				v.x += (drive_force / rb.mass) * Time.fixedDeltaTime;
+                rb.AddForce(drive_force*Vector3.left);
             }
             if (cmds.right) {
-				rb.AddForce(drive_force*Vector3.right);
-				v.x -= (drive_force / rb.mass) * Time.fixedDeltaTime;
+                rb.AddForce(drive_force*Vector3.right);
             }
-			update_speed ();
 
-			//radius of collision warning
-			float radius = 10;
-			Collider[] nearby = Physics.OverlapSphere (rb.position, radius);
+            //radius of collision warning
+            float radius = 10;
+            Collider[] nearby = Physics.OverlapSphere(rb.position, radius);
+            danger = false;
 
-			for (int i = 0; i < nearby.Length; i++) {
-				float angle = Vector3.Angle (nearby [i].attachedRigidbody.position, rb.position);
-				angle -= 90f;
+            for (int i = 0; i < nearby.Length; i++) {
+                float angle = Vector3.Angle(nearby[i].ClosestPointOnBounds(rb.position) - rb.position, Vector3.forward);
 
-				if (-15f <= angle && angle <= 15f)
-					danger = true;
-			}
+                if (angle <= 15f) {
+                    danger = true;
+                }
+            }
+            update_speed();
         }
 
         if (!waitingForCommands) {
             PlayerData data;
-			data.drive_force = drive_force;
+            data.drive_force = drive_force;
             gm.inter.NewData(data);
             waitingForCommands = true;
         }
@@ -82,8 +74,7 @@ public class PlayerMovement : MonoBehaviour {
         gm.inter.EndLevel(data);
     }
 
-	void update_speed (){
-		//not being passed to the text display for some reason
-		speedText.text = (v.magnitude).ToString () + " m/s";
-	}
+    void update_speed (){
+        speedText.text = (rb.velocity.magnitude).ToString() + " m/s " + (danger ? "danger" : "");
+    }
 }

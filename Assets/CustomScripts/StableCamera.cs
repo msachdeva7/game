08c146fb236340e7875030c8b3f0da5a6c6f538b
Vladimir Mikulik <yordun@gmail.com>
2018-03-17 20:57:40 +0000

    Improved camera.
    The camera now follows the heading of the car, rather than its direction, making for a superior drifting experience.

--- Assets/CustomScripts/StableCamera.cs
+++ Assets/CustomScripts/StableCamera.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class StableCamera : MonoBehaviour {
 
-	public GameObject TheCar;
+	public GameObject TheCar;     // set in editor, this is the player Car object
+	public float rotationDamping; // set in editor, suggested value 10
+	private Rigidbody m_RigidBody;
 
+	private void Start() {
+		m_RigidBody = TheCar.GetComponent<Rigidbody> ();
+	}
 
 	void Update () {
-		// counteract rotation of the car model
-		transform.eulerAngles = new Vector3(0, TheCar.transform.eulerAngles.y, 0);
+
+		float actual_heading = (m_RigidBody.velocity.sqrMagnitude > 0.1 ?
+			Quaternion.LookRotation (m_RigidBody.velocity, Vector3.up).eulerAngles.y
+		:
+			m_RigidBody.transform.eulerAngles.y
+		                );
+
+		Quaternion desiredHeadingQ = Quaternion.Euler (0, actual_heading, 0);
+
+		// smoothly rotate towards current heading
+		transform.rotation = Quaternion.Lerp (transform.rotation, desiredHeadingQ, Time.deltaTime * rotationDamping);
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); //steady the camera
 	}
 }

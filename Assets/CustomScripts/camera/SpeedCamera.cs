9d94662c49aa3bde4248eb0685bcbb569d384601
Vladimir Mikulik <yordun@gmail.com>
2018-04-27 17:45:44 +0100

    improved camera so car looks faster

--- Assets/CustomScripts/camera/SpeedCamera.cs
+++ Assets/CustomScripts/camera/SpeedCamera.cs
@@ -9,22 +9,33 @@ public class SpeedCamera : MonoBehaviour {
 	private float TopSpeed;
 
 	private float slowDistance = 5;
-	private float fastDistance = 4;
+	private float slowHeight = 1.5f;
+	private float slowRotation = 12;
 	private float slowFoV = 60;
+	private float fastDistance = 4;
+	private float fastHeight = 0.8f;
+	private float fastRotation = 0;
 	private float fastFoV = 90;
+	private Vector3 slowPos; 
+	private Vector3 fastPos;
 
 	void Start () {
 		TopSpeed = TheCar.GetComponent<CarController> ().MaxSpeed;
+
+		slowPos = new Vector3(0, slowHeight, -slowDistance);
+		fastPos = new Vector3(0, fastHeight, -fastDistance);
 	}
 
 	void Update () {
 		float speed = TheCar.GetComponent<CarController> ().CurrentSpeed;
 		float relativeSpeed = speed / TopSpeed;
-		float dist = slowDistance + (fastDistance - slowDistance) * relativeSpeed;
-		float fov = slowFoV + (fastFoV - slowFoV) * relativeSpeed;
+
+		Vector3 pos = Vector3.Lerp  (slowPos,      fastPos,      relativeSpeed);
+		float fov = Mathf.Lerp      (slowFoV,      fastFoV,      relativeSpeed);
+		float rot = Mathf.LerpAngle (slowRotation, fastRotation, relativeSpeed);
+
 		Camera.main.fieldOfView = fov;
-		Vector3 localPos = Camera.main.transform.localPosition;
-		localPos.z = -dist;
-		Camera.main.transform.localPosition = localPos;
+		Camera.main.transform.localPosition = pos;
+		Camera.main.transform.localEulerAngles = new Vector3(rot, 0, 0);
 	}
 }

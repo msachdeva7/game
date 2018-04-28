36f9db6d4135414b7f487febf14e6309458a612f
Vladimir Mikulik <yordun@gmail.com>
2018-04-28 13:45:20 +0100

    Made camera more dynamic

--- Assets/CustomScripts/camera/SpeedCamera.cs
+++ Assets/CustomScripts/camera/SpeedCamera.cs
@@ -8,6 +8,7 @@ public class SpeedCamera : MonoBehaviour {
 	public GameObject TheCar;
 	private float TopSpeed;
 
+
 	private float slowDistance = 5;
 	private float slowHeight = 1.5f;
 	private float slowRotation = 12;
@@ -19,20 +20,37 @@ public class SpeedCamera : MonoBehaviour {
 	private Vector3 slowPos; 
 	private Vector3 fastPos;
 
+	private float currentSlide;
+	private float maxSlide = 2;
+	private float slideLerpCoeff = 0.5f;
+	private float slideAngleCoeff = 0.05f;
+
 	void Start () {
 		TopSpeed = TheCar.GetComponent<CarController> ().MaxSpeed;
 
 		slowPos = new Vector3(0, slowHeight, -slowDistance);
 		fastPos = new Vector3(0, fastHeight, -fastDistance);
+
+		currentSlide = 0;
 	}
 
-	void Update () {
+	void FixedUpdate () {
+
+		float rigAngle = transform.localEulerAngles.y;
+		if (Mathf.Abs (rigAngle) > 180) {
+			rigAngle = -(360 - rigAngle);
+		}
+		float targetSlide = Mathf.Clamp (rigAngle * slideAngleCoeff, -maxSlide, maxSlide);
+		currentSlide = Mathf.Lerp (currentSlide, targetSlide, slideLerpCoeff);
+
 		float speed = TheCar.GetComponent<CarController> ().CurrentSpeed;
 		float relativeSpeed = speed / TopSpeed;
 
-		Vector3 pos = Vector3.Lerp  (slowPos,      fastPos,      relativeSpeed);
 		float fov = Mathf.Lerp      (slowFoV,      fastFoV,      relativeSpeed);
 		float rot = Mathf.LerpAngle (slowRotation, fastRotation, relativeSpeed);
+
+		Vector3 pos = Vector3.Lerp  (slowPos,      fastPos,      relativeSpeed);
+		pos.x = -currentSlide;
 
 		Camera.main.fieldOfView = fov;
 		Camera.main.transform.localPosition = pos;
